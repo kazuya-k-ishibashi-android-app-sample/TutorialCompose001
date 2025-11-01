ca1dd2a05c1feac4db69557d473578779ebee005
kazuya-k-ishibashi <kazuya.ishibashi.wn@gmail.com>
2025-11-01 21:15:03 +0900

    Text
    最小限のText。
    edge-to-edgeのため、画面端に表示され、ステータスバーと重なる。

--- app/src/main/java/com/kishibashi/androidapp/sample/compose/tutorial001/MainActivity.kt
+++ app/src/main/java/com/kishibashi/androidapp/sample/compose/tutorial001/MainActivity.kt
@@ -4,44 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kishibashi.androidapp.sample.compose.tutorial001.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // edge-to-edge
+        // https://developer.android.com/develop/ui/views/layout/edge-to-edge
+        // https://developer.android.com/develop/ui/compose/system/insets
         enableEdgeToEdge()
+
         setContent {
-            AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Text("Hello Android!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
