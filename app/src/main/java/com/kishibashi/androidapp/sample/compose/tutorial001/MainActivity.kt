7ee45d3e51537c9cc0f1fe3fbab42c22f92ce8a5
kazuya-k-ishibashi <kazuya.ishibashi.wn@gmail.com>
2025-11-01 23:10:15 +0900

    Modifier

--- app/src/main/java/com/kishibashi/androidapp/sample/compose/tutorial001/MainActivity.kt
+++ app/src/main/java/com/kishibashi/androidapp/sample/compose/tutorial001/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            Text("Hello Android!")
+            Text(
+                "Hello Android!",
+                color = Color.Red,
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .padding(horizontal = 16.dp, vertical = 32.dp)
+            )
         }
     }
 }
